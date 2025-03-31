874a633ff7a3548e8fa2ee6b13d2710a97b4d19a
Ilya Gaifullin <proskmax@yandex.ru>
2025-03-31 19:45:07 +0500

    base_class_Add

--- files/urls.py
+++ files/urls.py
@@ -5,7 +5,10 @@ app_name = 'files'
 
 urlpatterns = [ path('', views.FilesListView.as_view(), name='files_list'),
                 path('files/create/', views.FileCreateView.as_view(), name= 'files_create'),
-                path('file/<int:pk>', views.FileDetailView.as_view(), name= 'files_detail')
+                path('files/<int:pk>', views.FileDetailView.as_view(), name= 'files_detail'),
+                # path('file/update/<int:pk>', views.FileUpdateView.as_view(), name= 'files_update')
+                # path('files/delete/<int:pk>', views.FileDeleteView.as_view(), name= 'files_delete')
+
 
 
 ]
--- files/views.py
+++ files/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from files.forms import FilesForm
 from files.models import Files
@@ -17,6 +19,21 @@ class FileCreateView(CreateView):
     success_url = reverse_lazy('files:files_list')
     form_class = FilesForm
 
+
 class FileDetailView(DetailView):
     model = Files
+
+
+# class FileUpdateView(UpdateView):
+#     model = Files
+#     success_url = reverse_lazy('files:files_list')
+
+
+# class FileDeleteView(PermissionRequiredMixin, DeleteView):
+#     model = Files
+#     template_name = 'files/files_confirm_delete.html'
+#     success_url = reverse_lazy('files:files_list')
+#     permission_required = 'files.delete_file'
+
+
 
